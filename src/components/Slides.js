79c67d3e0836eca32339e0e59537f18387d85eba
Najeeb Khan <khannajeeb362@gmail.com>
2021-10-14 13:12:42 +0500

    intial commit

---
+++ src/components/Slides.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function Slides() {
+    return (
+        <div>
+            <div id="navigation" className="text-center">
+                <button data-testid="button-restart" className="small outlined" >Restart</button>
+                <button data-testid="button-prev" className="small" >Prev</button>
+                <button data-testid="button-next" className="small">Next</button>
+            </div>
+            <div id="slide" className="card text-center">
+                <h1 data-testid="title"></h1>
+                <p data-testid="text"></p>
+            </div>
+        </div>
+    );
+
+}
+
+export default Slides;
